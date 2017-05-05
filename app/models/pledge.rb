5f40abe31ea4e38a1bd32bb0be769c7869e8579f
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-04 21:02:35 -0400

    Projects have an owner and owners can't back their own projects

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,14 +4,7 @@ class Pledge < ApplicationRecord
   has_one :project, through: :reward
 
   validate :enough_pledged
-  validate :not_owner
-
-  # def not_owner
-  #   if self.user_id == current_user.id
-  #     self.errors.add(:dollar_amount, "The project owner cannot pledge")
-  #   end
-  # end
-
+  
   def enough_pledged
     unless self.dollar_amount >= reward.dollar_amount
       self.errors.add(:dollar_amount, "Amount pledged must be at least $#{reward.dollar_amount}")
